af423c207761e734b2218f503739ed6e644bb9b3
Tripathi <adarshtripathi.iitr@gmail.com>
2022-09-25 14:55:16 +0530

    Updates

--- src.e31bb0bc.js
+++ src.e31bb0bc.js
@@ -70333,7 +70333,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "41177" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "44973" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
